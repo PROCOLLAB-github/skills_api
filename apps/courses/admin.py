d7eec567000074dc1b1ea92b649d783ee9639e5d
Marsel Narbekov <lllmarselll@gmail.com>
2024-05-14 15:33:24 +0300

    Админка - "Части задачи" 1. Добавил необходимое поле "тип единицы задачи". 2. Добавил перевод в заголовке. 3. Чуть-чуть поправил verbose_name у модели (была опечатка)

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from courses.models import Skill, Task, TaskObject
+from questions.models import QuestionSingleAnswer
 
 
 @admin.register(Skill)
@@ -19,8 +20,19 @@ class TaskObjectAdmin(admin.ModelAdmin):
     list_display = (
         "id",
         "task_name",
+        "question_type",
         "ordinal_number",
     )
+    list_filter = ["task__name"]
 
     def task_name(self, obj):
         return obj.task.name
+
+    def question_type(self, obj):
+        if isinstance(obj.content_object, QuestionSingleAnswer) and obj.content_object.is_exclude:
+            return f"{obj.content_type} (Исключающий)"
+        else:
+            return obj.content_type
+
+    question_type.short_description = "тип единицы задачи"
+    task_name.short_description = "наименование задачи"
