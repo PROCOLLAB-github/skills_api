3bd8908a1691c66130548c41c99515fadaa113f9
Marsel Narbekov <lllmarselll@gmail.com>
2024-05-16 17:59:07 +0300

    Обработка исключений на проверке вопроса
    По задаче:
    1. CheckQuestionTypePermission доработан, чтобы работал и получением
    вопроса и с проверкой ответов
    2. Добавлен SimpleCheckQuestionTypePermission, для обработки:
    - QuestionWritePost
    - InfoSlidePost
    3. Добавлена функция: get_error_message_for_permissions
    Получить сообщение об ошибку, чтобы не дублировать код
    4. get_fields_for_answer_type
    Изменена логика и имя: Сейчас стала более гибкой, для новых типов
    вопросов/правок в старые, достаточно добавить/поменять field_for_prefetch
    5.wrong_endpoint_text
    Немного поправил, т.к. раньше не отрабатывала логика для определения
    конкретного типа QuestionSingleAnswer, если запрос летит из него же.
    
    Дополнительные задачи в процессе:
    1. SingleCorrectPost обрабатывал ответы немного некорректно, правки
    внесены.
    2. Где был, проставил тайп хинты
    
    Важные моменты, поостри, пожалуйста:
    1. apps/questions/views:
    В предыдущем коммите оставлял сообщение, там в ответе где-то
    использован id задачи, где-то id вопроса, где-то вообще ничего.
    Там были оставлены комментарии, если я в предположениях ошибся,
    то уберу(исходная логика не менялась).
    Возможно стоит к одному виду привести все ответы.
    
    2. По QuestionExcludePost писал в личку, работает кажется некорректно.

--- apps/questions/mapping.py
+++ apps/questions/mapping.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Union
 
-from questions.models import QuestionSingleAnswer
+from questions.models import QuestionSingleAnswer, InfoSlide, QuestionConnect, QuestionWrite
 
 
 class TypeQuestionPoints(Enum):
@@ -20,25 +20,26 @@ TaskObjs = Union[
 ]
 
 
-def get_answer_type(view_class) -> str:
-    view_name = view_class.__class__.__name__.lower()
-    answer_field = "content_object__"
-
-    if "exclude" in view_name or "single" in view_name:
-        answer_field += "single_answers"
-    elif "connect" in view_name:
-        answer_field += "connect_answers"
-
-    return answer_field
+def get_fields_for_answer_type(view_class) -> list[str]:
+    """Получение списка всех необходимых связанных объктов и полей для prefetch_related."""
+    field_for_prefetch = {
+        QuestionSingleAnswer: ["content_object", "content_object__single_answers"],
+        QuestionConnect: ["content_object", "content_object__connect_answers"],
+        QuestionWrite: ["content_object"],
+        InfoSlide: ["content_object"],
+    }
+    question_field: list[str] = field_for_prefetch.get(view_class.expected_question_model)
+    return question_field
 
 
 def wrong_endpoint_text(request_question, view) -> tuple[str, str]:
-    needed = view.expected_question_model._meta.verbose_name
-    gotten = request_question._meta.verbose_name
+    needed: str = view.expected_question_model._meta.verbose_name
+    gotten: str = request_question._meta.verbose_name
 
     view_name = view.__class__.__name__.lower()
     if isinstance(request_question, QuestionSingleAnswer):
         gotten = "Вопрос на исключение" if request_question.is_exclude else "Вопрос с одним правильным ответом"
+    if view.expected_question_model == QuestionSingleAnswer:
         if "exclude" in view_name:
             needed = "Вопрос на исключение"
         elif "single" in view_name:
--- apps/questions/permissions.py
+++ apps/questions/permissions.py
@@ -3,47 +3,39 @@ from rest_framework import permissions
 from rest_framework.exceptions import PermissionDenied
 
 from courses.models import TaskObject
-from questions.mapping import get_answer_type, wrong_endpoint_text
+from questions.mapping import get_fields_for_answer_type, wrong_endpoint_text
+from questions.services import get_error_message_for_permissions
 
 
 class CheckQuestionTypePermission(permissions.BasePermission):
     """
-    Проверка типа вопроса для GET эндпоинта вопросов, если запрос прилетел не туда,
+    Проверка типа вопроса для эндпоинтов вопросов, если запрос прилетел не в тот тип вопроса,
     то резится PermissionDenied пояснением, что необходимо было дернуть.
     """
 
     def has_permission(self, request, view):
         task_object_id = view.kwargs.get("task_obj_id")
+        prefetch_fields_list: list[str] = get_fields_for_answer_type(view)
+        # Для GET запроса необходимо подягивать файлы, для POST нет.
+        if view.view_request_type == "get":
+            prefetch_fields_list.append("content_object__files")
+
         try:
             request_task_object: TaskObject = get_object_or_404(
-                TaskObject.objects.prefetch_related(
-                    "content_object",
-                    get_answer_type(view),
-                    "content_object__files",
-                ),
+                TaskObject.objects.prefetch_related(*prefetch_fields_list),
                 id=task_object_id,
             )
         except AttributeError as e:
             error_message = str(e.args).lower()
             if "cannot find" in error_message and "is an invalid parameter to prefetch_related()" in error_message:
                 request_question = (
-                    TaskObject.objects.prefetch_related(
-                        "content_object",
-                    )
+                    TaskObject.objects.prefetch_related("content_object")
                     .get(id=task_object_id)
                     .content_object
                 )
-
                 needed_model_class, gotten_model_class = wrong_endpoint_text(request_question, view)
-                raise PermissionDenied(
-                    {
-                        "error": (
-                            f"You tried to summon taskobject with a wrong endpoint. "
-                            f"Instead of using '{needed_model_class}' endpoint, "
-                            f"try using '{gotten_model_class}' endpoint."
-                        )
-                    }
-                )
+                error_message = get_error_message_for_permissions(needed_model_class, gotten_model_class)
+                raise PermissionDenied(error_message)
             else:
                 raise AttributeError(str(e))
 
@@ -54,12 +46,25 @@ class CheckQuestionTypePermission(permissions.BasePermission):
             view.task_object_id = task_object_id
             view.request_question = request_question
             return True
-        raise PermissionDenied(
-            {
-                "error": (
-                    f"You tried to summon taskobject with a wrong endpoint. "
-                    f"Instead of using '{needed_model_class}' endpoint, "
-                    f"try using '{gotten_model_class}' endpoint."
-                )
-            }
+        error_message = get_error_message_for_permissions(needed_model_class, gotten_model_class)
+        raise PermissionDenied(error_message)
+
+
+class SimpleCheckQuestionTypePermission(permissions.BasePermission):
+    """
+    Упрощенная проверка(там где не требуются связанные объекты) типа вопроса для эндпоинтов вопросов, если запрос
+    прилетел не в тот тип вопроса, то резится PermissionDenied пояснением, что необходимо было дернуть.
+    """
+    def has_permission(self, request, view):
+        task_object_id = view.kwargs.get("task_obj_id")
+        request_task_object: TaskObject = get_object_or_404(
+            TaskObject.objects.prefetch_related("content_object"),
+            id=task_object_id,
         )
+        request_question = request_task_object.content_object
+        needed_model_class, gotten_model_class = wrong_endpoint_text(request_question, view)
+        if isinstance(request_question, view.expected_question_model) and needed_model_class == gotten_model_class:
+            view.task_object_id = task_object_id
+            return True
+        error_message = get_error_message_for_permissions(needed_model_class, gotten_model_class)
+        raise PermissionDenied(error_message)
