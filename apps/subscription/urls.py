d21dca6f0cd873c714634b79ecc95260837db925
Alexey Kudelko <flexivanov237@gmail.com>
2024-06-19 14:39:28 +0300

    refund function added

--- apps/subscription/urls.py
+++ apps/subscription/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from subscription.views import CreatePayment, ViewSubscriptions, ServeWebHook
+from subscription.views import CreatePayment, ViewSubscriptions, ServeWebHook, CreateRefund
 
 urlpatterns = [
     path("buy/", CreatePayment.as_view()),
     path("", ViewSubscriptions.as_view()),
     path("renew-sub-date", ServeWebHook.as_view()),
+    path("refund", CreateRefund.as_view()),
 ]
