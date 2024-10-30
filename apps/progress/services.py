3a70a1893b88acb57d75048f5512d5427749c1b6
Alexey Kudelko <flexivanov237@gmail.com>
2024-10-30 15:06:21 +0300

    added link field

--- apps/progress/services.py
+++ apps/progress/services.py
@@ -1,28 +1,34 @@
 import datetime
+import math
+from decimal import Decimal, ROUND_HALF_UP
 
-from django.db.models import Q, Count, QuerySet
+from django.db.models import Count, Q, QuerySet, Sum
+from django.utils import timezone
 
-from courses.models import Skill, Task
 from progress.mapping import MonthMapping
-from progress.models import UserProfile, IntermediateUserSkills
-from progress.typing import SkillIdType, SkillProgressType, SkillMonthProgressType
+from courses.models import Skill
+from progress.typing import (
+    UserProfileDataDict,
+    UserSkillsProgressDict,
+    UserMonthsProgressDict,
+    PreparatoryUserMonthsProgressDict,
+)
+from progress.models import (
+    UserMonthStat,
+    UserProfile,
+    UserWeekStat,
+    UserSkillDone,
+    TaskObjUserResult,
+)
 
 
-def months_passed_data() -> tuple[datetime.date, datetime.date]:
-    """Последние 2 месяца не включая текущий."""
-    today = datetime.date.today()
-    first = today.replace(day=1)
-    last_month = first - datetime.timedelta(days=1)
-
-    sec = last_month.replace(day=1)
-    last_last_month = sec - datetime.timedelta(days=1)
-
-    return last_last_month, last_month
-
-
-def get_user_data(profile_id: int) -> dict:
+def get_user_data(profile_id: int) -> UserProfileDataDict:
+    """
+    Информация о юзере для профиля:
+        Общие перснальные данные.
+        Баллы (`points`).
+    """
     user_profile = UserProfile.objects.select_related("user", "file").get(id=profile_id)
-    # TODO добавить сериалайзер
     user = user_profile.user
 
     years_passed = None
@@ -38,105 +44,61 @@ def get_user_data(profile_id: int) -> dict:
         "specialization": user.specialization,
         "age": years_passed,
         "geo_position": user.city,
+        "points": get_user_points(profile_id),
     }
 
 
-def get_current_level(user_profile_id: int) -> dict[SkillIdType, SkillProgressType]:
-    """Выдаёт наиболее маленький непройденный уровень у всех скиллов пользователя"""
-
-    # Навыки и id навыков пользователя.
-    user_skills, user_skills_ids = get_user_skills(user_profile_id)
-
-    # Получение значений для всех задач, связанных с навыками пользователя.
-    tasks: QuerySet = (
-        # TODO Fix как утвердится инфа для профиля, надо учесть недели.
-        Task.published.filter(skill__in=user_skills_ids)
-        .annotate(
-            num_questions=Count("task_objects"),
-            num_answers=Count(
-                "task_objects__user_results", filter=Q(task_objects__user_results__user_profile__id=user_profile_id)
-            ),
-        )
-        .values("skill_id", "level", "num_questions", "num_answers")
-    )
-
-    # Подготовительный словарь для хранения данных о навыках.
-    skills_data: dict[SkillIdType, SkillProgressType] = {
-        skill.id: {"skill_name": skill.name, "level": 1} for skill in user_skills
-    }
-
-    # Определение текущего уровня навыка на основе задач.
-    for task in tasks:
-        skill_id = task["skill_id"]
-        level = task["level"]
-        num_questions = task["num_questions"]
-        num_answers = task["num_answers"]
-
-        if skills_data[skill_id]["level"] == level:
-            if num_questions == num_answers:
-                skills_data[skill_id]["level"] += 1
-            else:
-                skills_data[skill_id]["progress"] = round((num_answers / num_questions) * 100)
-    return skills_data
-
-
-def last_two_months_stats(user_profile_id: int) -> list[SkillMonthProgressType]:
+def get_user_profile_skills_progress(user_profile_id: int) -> list[UserSkillsProgressDict]:
     """
-    Получение информации по навыкам за 2 последних месяца не включая текущий.
-    Если в течении месяца было закомпличено хотя бы по 1 уровню у ВСЕХ навыков, то True.
+    Статистика по навыкам пользователя с % прогресса для профиля. 
     """
-    two_months_ago, last_months = months_passed_data()
-    user_skills_ids: QuerySet = get_user_skills_data(user_profile_id, (two_months_ago, last_months))
-
-    # Получение значений для всех уровней за 2 месяца, связанных с навыками пользователя.
-    tasks: QuerySet = (
-        Task.published.filter(skill_id__in=user_skills_ids)
+    # TODO Fix учесть, когда будет выбор только 5 навыков для юзера.
+    _, user_skills_ids = get_user_skills(user_profile_id)
+    skills_stats: QuerySet[Skill] = (
+        Skill.published
+        .filter(id__in=user_skills_ids)
         .annotate(
-            num_questions=Count("task_objects"),
-            num_answers_last_months=Count(
-                "task_objects__user_results",
-                filter=Q(
-                    task_objects__user_results__user_profile__id=user_profile_id,
-                    task_objects__user_results__datetime_created__month=last_months.month,
-                    task_objects__user_results__datetime_created__year=last_months.year,
+                # Общее кол-во опубликованных вопросов навыка.
+                total_num_questions=Count("tasks__task_objects", filter=Q(tasks__status="published")),
+                # Общее кол-во ответов юзера на опубликованные вопросы в рамках навыка.
+                total_user_answers=Count(
+                    "tasks__task_objects__user_results",
+                    filter=(Q(tasks__task_objects__user_results__user_profile_id=user_profile_id)
+                            & Q(tasks__status="published")),
                 ),
-            ),
-            num_answers_two_months_ago=Count(
-                "task_objects__user_results",
-                filter=Q(
-                    task_objects__user_results__user_profile__id=user_profile_id,
-                    task_objects__user_results__datetime_created__month=two_months_ago.month,
-                    task_objects__user_results__datetime_created__year=two_months_ago.year,
-                ),
-            ),
-        )
-        .values("skill_id", "level", "num_questions", "num_answers_last_months", "num_answers_two_months_ago")
+            )
     )
-
-    # Подготовительный словарь с данными по навыку и комплиту хотя бы 1 уровня за 2 месяца.
-    stats: dict[SkillIdType, SkillMonthProgressType] = {}
-    for task in tasks:
-        skill_id = task["skill_id"]
-        num_questions = task["num_questions"]
-        if skill_id not in stats:
-            stats[skill_id] = {"last_months": False, "two_months_ago": False}
-        if num_questions == task["num_answers_last_months"]:
-            stats[skill_id]["last_months"] = True
-        if num_questions == task["num_answers_two_months_ago"]:
-            stats[skill_id]["two_months_ago"] = True
-
-    # Результирующий список словарей.
-    months_data: list[SkillMonthProgressType] = [
+    stats_data: list[UserSkillsProgressDict] = [
         {
-            "month": MonthMapping[last_months.month],
-            "is_passed": all(task["last_months"] for task in stats.values()) if stats else False,
-        },
-        {
-            "month": MonthMapping[two_months_ago.month],
-            "is_passed": all(task["two_months_ago"] for task in stats.values()) if stats else False,
-        },
+            "skill_id": skill.id,
+            "skill_name": skill.name,
+            "skill_level": 1,  # TODO Fix захардкоженный уровень, пока не понятно что за уровни навыка.
+            "skill_progress": get_rounded_percentage(skill.total_user_answers, skill.total_num_questions),
+            "file_link": skill.file.link if skill.file else None,
+        }
+        for skill in skills_stats
     ]
-    return months_data
+    return stats_data
+
+
+def get_user_profile_months_stats(user_profile_id: int) -> list[UserMonthsProgressDict]:
+    """Статистика по месяцам для пользователя для текущего года."""
+    now_year: int = timezone.now().year
+    months_data: list[PreparatoryUserMonthsProgressDict] = (
+        UserMonthStat.objects
+        .filter(user_profile__id=user_profile_id, year=now_year)
+        .values("month", "year", "successfully_done")
+    )
+    # Словари пересобирается через маппинг, чтобы вернуть `str` месяц.
+    months_stats: list[UserMonthsProgressDict] = [
+        {
+            "month": MonthMapping.get(stat["month"]),
+            "year": stat["year"],
+            "successfully_done": stat["successfully_done"]
+        }
+        for stat in months_data
+    ]
+    return months_stats
 
 
 def get_user_skills(user_profile_id: int) -> tuple[QuerySet[Skill], QuerySet]:
@@ -149,15 +111,59 @@ def get_user_skills(user_profile_id: int) -> tuple[QuerySet[Skill], QuerySet]:
     return user_skills, user_skills_ids
 
 
-def get_user_skills_data(user_profile_id: int, dates: tuple[datetime.date, datetime.date]) -> QuerySet:
-    """Получение всех навыков и их id для конкретного пользователя."""
-    user_skills_ids = (
-        IntermediateUserSkills.objects.select_related("skill")
-        .filter(
-            user_profile_id=user_profile_id,
-            date_chosen__year__in=[date.year for date in dates],
-            date_chosen__month__in=[date.month for date in dates],
-        )
-        .values_list("id", flat=True)
-    )
-    return user_skills_ids
+def get_user_available_week(profile_id: int) -> int:
+    """
+    Получение доступных пользователю недель, все что <= `available_week` доступно.
+    Если подписка вообще не оформлена, то -> 0, соотв. ничего не доступно.
+    Пример запроса для Task:
+        `Q(week__lte=available_week)`
+    """
+    subscription_date: datetime = UserProfile.objects.get(pk=profile_id).last_subscription_date
+
+    if subscription_date:
+        current_date: datetime = timezone.now().date()
+        # Если подписка была сегодня то прошло дней 0, сооотв -> 1.
+        days_since_subscription: datetime.timedelta = (current_date - subscription_date).days or 1
+        # Дробный результат преобразуется в бОльшую часть, пример: 2.1 -> 3
+        # (т.е. как началась 3 неделя, чтобы она была включительно)
+        available_week: int = math.ceil(days_since_subscription / 7)
+        return available_week
+    return 0
+
+
+def get_user_points(profile_id: int) -> int:
+    """Собирает поинты пользователя по всем сущностям."""
+
+    # TODO подумать можно ли это упростить, обратный вызов через `related_name`, distinct, annotate и Sum
+    # дублирует строки и стата перемножается.
+    skill_done_points: int = (
+        UserSkillDone.objects
+        .filter(user_profile__id=profile_id)
+        .aggregate(points=Sum("additional_points"))
+    )["points"] or 0
+
+    week_done_points: int = (
+        UserWeekStat.objects
+        .filter(user_profile__id=profile_id)
+        .aggregate(points=Sum("additional_points"))
+    )["points"] or 0
+
+    task_objects_pounts: int = (
+        TaskObjUserResult.objects
+        .filter(user_profile__id=profile_id)
+        .aggregate(points=Sum("points_gained"))
+    )["points"] or 0
+
+    return skill_done_points + week_done_points + task_objects_pounts
+
+
+def get_rounded_percentage(dividend: int, divisor: int) -> int:
+    """
+    Принимает делитель и делимое, возврщает округленное процентное соотношение.
+    Если делитель/делимое == `0` -> вернет просто `0` без выброса исключения.
+    """
+    if divisor == 0 or dividend == 0:
+        return 0
+    percentage: Decimal = Decimal(str((dividend / divisor) * 100))
+    rounded_percentage: int = int(percentage.quantize(Decimal("1"), rounding=ROUND_HALF_UP))
+    return rounded_percentage
