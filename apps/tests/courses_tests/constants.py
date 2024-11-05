d922c1f268456ae2111fc60b8a9c2d3df7e7b330
Marsel <lllmarselll@gmail.com>
2024-11-05 20:04:01 +0300

    Рефакторинг тестов. Все path приложения courses покрыты тестами.

---
+++ apps/tests/courses_tests/constants.py
@@ -0,0 +1,20 @@
+COURSES_ALL_PATHS: list[str] = [
+    "/courses/1",
+    "/courses/choose-skills/",
+    "/courses/skill-details/1",
+    "/courses/tasks-of-skill/1",
+    "/courses/task-result/1",
+    "/courses/all-skills/",
+]
+
+COURSES_NO_ACCESS_PATHS: list[str] = [
+    "/courses/1",
+    "/courses/choose-skills/",
+    "/courses/skill-details/1",
+    "/courses/tasks-of-skill/1",
+    "/courses/task-result/1",
+]
+
+COURSES_ACCESS_PATHS: list[str] = [
+    "/courses/all-skills/",
+]
