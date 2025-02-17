40ee310443b18036d91b09a0455ef9ff4440830c
Toksi86 <puma-yellow@yandex.ru>
2025-02-17 13:30:01 +0500

    Добавлены модели: Траектория, Месяц, ТректорияПользователя, Встречи. Каждая из них зарегистрирована в админ панели

--- apps/trajectories/apps.py
+++ apps/trajectories/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TrajectoriesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "trajectories"
+    verbose_name = "Траектории"
